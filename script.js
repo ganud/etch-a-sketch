891c16364245d11a145928004d9a1bd968d0a24b
Dundo <107737568+ganud@users.noreply.github.com>
2023-08-08 15:29:27 -0400

    basic grid appearance finished

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 // Flexbox the divs into a square grid
 const container = document.getElementsByClassName("container")[0];
 
-for(let i=0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add("square");
-    container.appendChild(square);
-
+// Create a 16x16 grid
+for(let j=0; j <16; j++){
+    const line = document.createElement('div');
+    line.classList.add("line");
+    for(let i=0; i < 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add("square");
+        line.appendChild(square);
+    }
+    container.appendChild(line);
 }
